2e31b38e53e179d89df16a6014fa86f34b583032
Heavyrisem <46806498+Heavyrisem@users.noreply.github.com>
2020-12-16 11:56:23 +0900

    Update unitChanger.js

--- unitChanger.js
+++ unitChanger.js
@@ -8,6 +8,16 @@ class UnitChangerAPI {
 		if (e == "-Infinity") return "0 " + s[0];
 		else return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
 	}
+	
+	Killo(number) {
+		number = parseInt(number);
+		let names = ["K", "M"];
+		
+		let e = Math.floor(Math.log(number) / Math.log(1000));
+		
+		if (e == "-Infinity") return "0 " + names[0];
+		else return (number / Math.pow(1000, Math.floor(e))).toFixed(2) + " " + names[e];
+	}
 }
 
 module.exports = new UnitChangerAPI();
